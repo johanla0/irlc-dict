d50081f6f646f9ff910be2386b8284b577394a10
johanla0 <idlhero@gmail.com>
2020-12-07 17:47:01 +0300

    Add frequency sort, refactored

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import requests
 app_key = ""
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
 # download stopwords corpus, you need to run it once
 nltk.download("stopwords")
@@ -26,15 +27,7 @@ def preprocess_text(text):
     return tokens
 
 
-def count_words(words):
-    counted = {}
-    for word in words:
-        counted.setdefault(word, 0)
-        counted[word] += 1
-    return counted
-
-
-def transl(words):
+def translate(words):
     translated = {}
     for word in words:
         translated.setdefault(word, '')
@@ -52,6 +45,40 @@ def transl(words):
     return translated
 
 
+def checkLevel(text, level):
+    filepath_a1 = './assets/lex_minimum_a1.txt'
+    dataset = set(text)
+    if len(dataset) == 0:
+        return
+    words_a1 = set()
+    with open(filepath_a1) as fp:
+        for line in fp:
+            words_a1.add(line.rstrip())
+    if level == 'a1':
+        return len(dataset.intersection(words_a1)) / len(dataset)
+    filepath_a2 = './assets/lex_minimum_a2.txt'
+    words_a2 = set()
+    with open(filepath_a2) as fp:
+        for line in fp:
+            words_a2.add(line.rstrip())
+    return len(dataset.intersection(words_a2)) / len(dataset)
+
+
+def count_words(words):
+    counted = {}
+    for word in words:
+        counted.setdefault(word, 0)
+        counted[word] += 1
+    return counted
+
+
+def checkFrequency(text):
+    x = count_words(text)
+    res = {k: v for k, v in sorted(
+        x.items(), key=lambda item: item[1], reverse=True)}
+    return res
+
+
 @app.route('/api/', methods=["POST"])
 def main_interface():
     response = request.get_json()
@@ -60,43 +87,27 @@ def main_interface():
     result2 = sorted(preprocess_text(response['text2']))
     result1_set = set(result1)
     result2_set = set(result2)
+    result['result1_a1'] = ''
+    result['result1_a2'] = ''
+    result['result2_a1'] = ''
+    result['result2_a2'] = ''
     checkboxLevel = response['checkboxLevel']
     if checkboxLevel:
-        filepath_a1 = '/home/johanla0/mysite/assets/lex_minimum_a1.txt'
-        filepath_a2 = '/home/johanla0/mysite/assets/lex_minimum_a2.txt'
-        words_a1 = set()
-        words_a2 = set()
-        with open(filepath_a1) as fp:
-            for line in fp:
-                words_a1.add(line.rstrip())
-        with open(filepath_a2) as fp:
-            for line in fp:
-                words_a2.add(line.rstrip())
-        result['result1_a1'] = ''
-        result['result1_a2'] = ''
-        result['result2_a1'] = ''
-        result['result2_a2'] = ''
-        if len(result1_set) > 0:
-            result['result1_a1'] = len(
-                result1_set.intersection(words_a1)) / len(result1_set)
-            result['result1_a2'] = len(
-                result1_set.intersection(words_a2)) / len(result1_set)
-        if len(result2_set) > 0:
-            result['result2_a1'] = len(
-                result2_set.intersection(words_a1)) / len(result2_set)
-            result['result2_a2'] = len(
-                result2_set.intersection(words_a2)) / len(result2_set)
+        result['result1_a1'] = checkLevel(result1, 'a1')
+        result['result1_a2'] = checkLevel(result1, 'a2')
+        result['result2_a1'] = checkLevel(result2, 'a1')
+        result['result2_a2'] = checkLevel(result2, 'a2')
     checkboxFrequency = response['checkboxFrequency']
     checkboxTranslate = response['checkboxTranslate']
     if checkboxFrequency:
-        result['result1'] = count_words(result1)
-        result['result2'] = count_words(result2)
+        result['result1'] = checkFrequency(result1)
+        result['result2'] = checkFrequency(result2)
     else:
         result['result1'] = sorted(list(result1_set))
         result['result2'] = sorted(list(result2_set))
     if checkboxTranslate:
-        result['result1'] = transl(sorted(list(result1_set)))
-        result['result2'] = transl(sorted(list(result2_set)))
+        result['result1'] = translate(sorted(list(result1_set)))
+        result['result2'] = translate(sorted(list(result2_set)))
     result['intersection'] = []
     if len(result2) > 0:
         inters = result1_set.intersection(result2_set)
@@ -114,3 +125,4 @@ def add_headers(response):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
